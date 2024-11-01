7dc72464461702bf6f1ff4b6aee4b3d0dafd7115
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2024-10-31 23:49:04 -0400

    Initial Commit

---
+++ LocationPushTest/LocationPushTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationPushTestApp.swift
+//  LocationPushTest
+//
+//  Created by Fevin Felix on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationPushTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocationPushTestTests/LocationPushTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocationPushTestTests.swift
+//  LocationPushTestTests
+//
+//  Created by Fevin Felix on 10/31/24.
+//
+
+import Testing
+@testable import LocationPushTest
+
+struct LocationPushTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
